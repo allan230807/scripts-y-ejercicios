b88b661f027b30dbc87dcdd00f9c11206b47907e
allan230807 <ararciahurtado@gmail.com>
2026-07-27 18:31:27 -0400

    feat: add password creator script

---
+++ password_creator.py
@@ -0,0 +1,17 @@
+import random 
+
+def generate_password(length=10):
+    if length < 10:
+        raise ValueError("Password length should be at least 10 characters.")
+    characters = "asdfghjklpoiuytrewqzxcvbnmASDFGHJKLPOIUYTREWQZXCVBNM1234567890!@#$%^&*()_+-=[]{}|;:',.<>/?"
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+if __name__ == "__main__":
+        try:
+            user_length = int(input("Ingresa la longitud de la contraseña (mínimo 10): "))
+            print("Tu contraseña generada es:", generate_password(user_length))
+        except ValueError as e:
+            print(f"Error: {e}")
+
+  
+
