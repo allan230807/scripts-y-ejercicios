0226137ad6d5a908b3ecd12b834ced7f9a5d1f71
allan230807 <ararciahurtado@gmail.com>
2026-08-02 20:22:40 -0400

    ejercicio 2

---
+++ HACKERRANK/2.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    suma = a + b
+    print(suma)
+    
+    diferencia = a - b 
+    print(diferencia)
+    
+    producto = a * b
+    print(producto)
