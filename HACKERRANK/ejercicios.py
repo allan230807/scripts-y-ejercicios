839ba7d462da26d0a670a1e22a9d3a250f197937
allan230807 <ararciahurtado@gmail.com>
2026-08-03 19:06:59 -0400

    ejercicio 10

--- HACKERRANK/ejercicios.py
+++ HACKERRANK/ejercicios.py
@@ -29,3 +29,13 @@ if __name__ == '__main__':
     result = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
     
     print(result)
+
+    #ejercicio 10
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    unique_scores = sorted(set(arr))
+    print(unique_scores[-2])
+        
+    
