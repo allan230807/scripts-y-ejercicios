17f5b0601dd18755b15fefaf3b8f7941dafac97f
allan230807 <ararciahurtado@gmail.com>
2026-08-03 20:12:25 -0400

    ejercicio 11

--- HACKERRANK/ejercicios.py
+++ HACKERRANK/ejercicios.py
@@ -37,5 +37,20 @@ if __name__ == '__main__':
     arr = map(int, input().split())
     unique_scores = sorted(set(arr))
     print(unique_scores[-2])
+
+#ejercicio 11
+if __name__ == '__main__':
+    students = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name, score])
+    scores = sorted(set(student[1] for student in students))
+    second_lowest_score = scores[1]
+    second_lowest_students = sorted([
+        student[0] for student in students if student[1] == second_lowest_score
+    ])
+    for name in second_lowest_students:
+        print(name)
         
     
